d20ed2ce26d21f6d401555c244c80351baae2f58
MYSBS <qjatnekt567@gmail.com>
2025-06-11 00:57:21 +0900

    트리의 부모

--- 202506_2/BOJ11725.py
+++ 202506_2/BOJ11725.py
@@ -6,31 +6,23 @@ N = int(input())
 num_list = deque([])
 par = [0] * (N+1)
 
+adj_list = [[] for _ in range(N+1)]
 for _ in range(N-1):
-    p, c = map(int, input().split())
-    if p == 1:
-        par[c] = p
-    elif c == 1:
-        par[p] = c
-    elif par[p] != 0:
-        par[c] = p
-    elif par[c] != 0:
-        par[p] = c
-    else:
-        num_list.append((p, c))
+    a, b = map(int, input().split())
+    adj_list[a].append(b)
+    adj_list[b].append(a)
 
 
-while num_list:
-    pair = num_list.popleft()
-    p , c = pair
-    if par[p] == 0 and par[c] == 0:
-        num_list.append((p, c))
-        continue
-    else:
-        if par[p] != 0:
-            par[c] = p
-        else:
-            par[p] = c
+queue = deque([1])
+par[1] = 1  
+
+
+while queue:
+    cur = queue.popleft()
+    for nxt in adj_list[cur]:
+        if par[nxt] == 0:
+            par[nxt] = cur
+            queue.append(nxt)
         
 
 for num in par[2:]:
