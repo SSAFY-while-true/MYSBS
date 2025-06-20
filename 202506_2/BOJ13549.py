7509152cd357146c45e9532755a4592a69d13435
MYSBS <qjatnekt567@gmail.com>
2025-06-20 20:04:12 +0900

    숨바꼭질3

--- 202506_2/BOJ13549.py
+++ 202506_2/BOJ13549.py
@@ -9,24 +9,18 @@ def my_bfs(N, K):
     while dq:
         cur_time = dq.popleft()
 
-        if cur_time == K:
-            return visited[cur_time]
-
         next_time = cur_time * 2
         if 0 <= next_time < 100001 and visited[next_time] == -1:
             visited[next_time] = visited[cur_time]
             dq.appendleft(next_time)
 
-        next_time = cur_time + 1
-        if 0 <= next_time < 100001 and visited[next_time] == -1:
-            visited[next_time] = visited[cur_time] + 1
-            dq.append(next_time)
+        for next_time in [cur_time - 1, cur_time + 1]:
+            if 0 <= next_time < 100001 and visited[next_time] == -1:
+                visited[next_time] = visited[cur_time] + 1
+                dq.append(next_time)
 
+    return visited[K]
 
-        next_time = cur_time - 1
-        if 0 <= next_time < 100001 and visited[next_time] == -1:
-            visited[next_time] = visited[cur_time] + 1
-            dq.append(next_time)
 
 N, K = map(int, input().split())
 print(my_bfs(N, K))
